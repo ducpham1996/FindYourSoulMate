536d7bfbd4cb8c6712456efac31af28d2b4b7732
Duc\Ducc <ducpagc00970@fpt.edu.vn>
2018-05-16 13:50:26 +0700

    init

---
+++ testconnection.php
@@ -0,0 +1,7 @@
+<?php
+   // connect to mongodb
+   $m = new MongoDB\Driver\Manager("mongodb://<findsoulmate>:<!duc12345>@den1.mongo1.gear.host:27001/findsoulmate");
+	
+   echo "Connection to database successfully";
+   // select a database
+?>
