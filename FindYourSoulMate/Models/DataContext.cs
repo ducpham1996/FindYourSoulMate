a5af935e3dfdc6ff800fee19c377d7ed3d9591fa
Duc\Ducc <ducpagc00970@fpt.edu.vn>
2018-05-19 13:41:46 +0700

    init

---
+++ FindYourSoulMate/Models/DataContext.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindYourSoulMate.Models
+{
+    public class DataContext
+    {
+        public async Task testConnectionAsync()
+        {
+            MongoClient dbClient = new MongoClient("mongodb://findsoulmate:!duc12345@den1.mongo1.gear.host:27001/findsoulmate");
+            IMongoDatabase db = dbClient.GetDatabase("findsoulmate");
+            var collection = db.GetCollection<BsonDocument>("Post");
+            Debug.WriteLine("The list of collections are :");
+            using (var cursor = await collection.Find(new BsonDocument()).ToCursorAsync())
+            {
+                while (await cursor.MoveNextAsync())
+                {
+                    foreach (var doc in cursor.Current)
+                    {
+                        Debug.WriteLine(doc);
+                    }
+                }
+            }
+        }
+    }
+}
